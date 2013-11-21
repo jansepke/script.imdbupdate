c9c618818dbda8a59f1393aaf4cc72ecef913339
Jan <Jan@172.18.1.24>
2013-11-21 18:42:08 +0100

    v0.4.6 (2013-11-21): - Bug fixes

--- resources/lib/imdbMovie.py
+++ resources/lib/imdbMovie.py
@@ -6,11 +6,13 @@
 import httplib, socket, json
 
 class imdbMovie(object):
+    
     def __init__(self, imdbID, httphandler):
         self.__rating = ""
         self.__votes = ""
         self.__error = False
         self.__imdbID = imdbID
+        
         self.getData(httphandler)
 
     def getData(self, httphandler):
@@ -21,12 +23,15 @@ class imdbMovie(object):
             self.__error = True
         else:
             if response.status == 200:
-                data = json.loads(response.read().decode('utf8'))
-                if "error" in data or data["Response"] == "False":
+                try:
+                    data = json.loads(response.read().decode('utf8'))
+                    if "error" in data or data["Response"] == "False":
+                        self.__error = True
+                    else:
+                        self.__rating = data["imdbRating"]
+                        self.__votes = str(data["imdbVotes"])
+                except:
                     self.__error = True
-                else:
-                    self.__rating = data["imdbRating"]
-                    self.__votes = str(data["imdbVotes"])
             else:
                 self.__error = True
     
