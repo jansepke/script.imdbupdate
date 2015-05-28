03477e3f2c53c8860af2b4f2f6b4e6b65367dcf6
Jandalf <jandalf2@gmail.com>
2015-05-28 22:32:33 +0200

    fix: gruntfile changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,11 @@
-var path = require('path');
+var path = require('path')
+loadTasks = require('load-grunt-tasks')
 
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+    loadTasks(grunt)
 
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         watch: {
             scripts: {
                 files: ['addon/**'],
@@ -15,10 +17,49 @@ module.exports = function(grunt) {
                 files: [{
                     cwd: 'addon',
                     src: ['**'],
-                    dest: path.join(process.env['HOME'], '/AppData/Roaming/Kodi/addons/script.imdbupdate')
+                    dest: path.join(process.env['HOME'], '/AppData/Roaming/Kodi/addons/<%= pkg.name %>')
                 }],
                 verbose: true
             }
+        },
+        compress: {
+            build: {
+                options: {
+                    archive: 'build/<%= pkg.name %>-<%= pkg.version %>.zip'
+                },
+                files: [{
+                    cwd: 'addon',
+                    src: ['**'],
+                    expand: true
+                }]
+            }
+        },
+        xmlpoke: {
+            bump: {
+                options: {
+                    xpath: '/addon/@version',
+                    value: '<%= pkg.version %>'
+                },
+                files: {
+                    'addon/addon.xml': 'addon/addon.xml'
+                },
+            },
+        },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: ['pkg'],
+                commit: false,
+                createTag: false,
+                push: false
+            }
+        },
+        changelog: {
+            options: {
+                from: 'db8de4f7c0788454a86dab8a1f335a9d20363118'
+            }
         }
-    });
-};
+    })
+
+    grunt.registerTask('build', ['bump', 'xmlpoke', 'compress']);
+}
