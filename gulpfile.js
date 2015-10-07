80c7d78c69bea3c720e275e1d14ea73b4f9e0132
Jan Sepke <jandalf2@gmail.com>
2015-10-07 23:22:58 +0200

    chore: switch from grunt to gulp

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp'),
+  zip = require('gulp-zip'),
+  util = require('util'),
+  runSequence = require('run-sequence'),
+  bump = require('gulp-bump'),
+  cheerio = require('gulp-cheerio'),
+  release = require('gulp-github-release');
+
+gulp.task('compress', function () {
+  var pkg = require('./package.json');
+  var fileName = util.format('script.imdbupdate-%s.zip', pkg.version);
+
+  return gulp.src('script.imdbupdate/**', { base: __dirname })
+    .pipe(zip(fileName))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('bump', function () {
+  return gulp.src('./package.json')
+    .pipe(bump())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('xml', function () {
+  var pkg = require('./package.json');
+
+  return gulp
+    .src(['script.imdbupdate/addon.xml'])
+    .pipe(cheerio({
+      run: function ($, file) {
+        $('addon').attr('version', pkg.version);
+      },
+      parserOptions: {
+        xmlMode: true
+      }
+    }))
+    .pipe(gulp.dest('script.imdbupdate'));
+});
+
+gulp.task('build', function (callback) {
+  runSequence('bump', 'xml', 'compress', callback);
+});
+
+gulp.task('github-release', function () {
+  var pkg = require('./package.json');
+  var fileName = util.format('script.imdbupdate-%s.zip', pkg.version);
+
+  return gulp.src('build/' + fileName)
+    .pipe(release({
+      owner: 'Jandalf',
+      repo: 'script.imdbupdate',
+      manifest: pkg
+    }));
+});
+
+gulp.task('release', function (callback) {
+  runSequence('build', 'github-release', callback);
+});
